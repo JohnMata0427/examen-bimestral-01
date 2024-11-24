8fe01076ec4b8cda8f8d6c2f12c514a89d284b07
JohnMata0427 <jhonmata0427@gmail.com>
2024-11-24 18:20:38 -0500

    Bugs fixed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,18 +9,32 @@ import { type Book } from 'src/app/services/api-consumption.service';
 })
 export class HomePage {
   public images: string[] = [];
-  public loading: boolean = false;
-  public books: Book[] = [];
   public openAlert: boolean = false;
   public message: string = '';
+  public loading!: boolean;
+  public books!: Book[];
 
   constructor(private apiConsumptionService: ApiConsumptionService) {}
 
-  ngOnInit() {
-    this.loading = true;
-    this.getFiveRandomDogs();
-    this.getFiveRandomRobots();
-    this.apiConsumptionService.getRandomBooks().subscribe({
+  ngOnInit(): void {
+    this.getNewBooks();
+  }
+
+  getDogImages(): void {
+    this.apiConsumptionService.getRandomDogs().subscribe({
+      next: ({ message }: { message: string[] }) => {
+        this.images = [...this.images, ...message];
+      },
+    });
+  }
+
+  getRobotImages(): void {
+    const robotImages = this.apiConsumptionService.getRandomRobots();
+    this.images = [...this.images, ...robotImages];
+  }
+
+  getBooks(): void {
+    this.apiConsumptionService.getRandomBooks(15).subscribe({
       next: ({ results }: { results: any[] }) => {
         this.books = results.map((book: any, index: number) => {
           return {
@@ -30,11 +44,7 @@ export class HomePage {
             thumbnail: this.images[index],
           };
         });
-
-        console.log(this.books);
-      },
-      error: (error: any) => {
-        console.log(error);
+        this.books = this.books.slice(0, 10);
       },
       complete: () => {
         this.loading = false;
@@ -42,37 +52,17 @@ export class HomePage {
     });
   }
 
-  getFiveRandomDogs() {
-    for (let i = 0; i < 5; i++) {
-      this.apiConsumptionService.getRandomDog().subscribe({
-        next: ({ message }: { message: string }) => {
-          this.images.push(message);
-        },
-        error: (error: any) => {
-          console.log(error);
-        },
-      });
-    }
+  getNewBooks(): void {
+    this.loading = true;
+    this.images = [];
+    this.getRobotImages();
+    this.getDogImages();
+    this.getBooks();
   }
 
-  getFiveRandomRobots() {
-    for (let i = 0; i < 5; i++) {
-      const robotUrl = this.apiConsumptionService.getRandomRobot();
-      this.images.push(robotUrl);
-    }
-  }
-  
-  reloadPage() {
-    window.location.reload();
-  }
-
-  async saveBooks() {
-    const result = await this.apiConsumptionService.saveBooksGenerated(this.books);
-    this.setOpenAlert(true);
+  async saveBooks(books: Book[]): Promise<void> {
+    const result = await this.apiConsumptionService.saveBooksGenerated(books);
+    this.openAlert = true;
     this.message = result;
-  }
-
-  setOpenAlert(openAlert: boolean) {
-    this.openAlert = openAlert;
   }
 }
--- src/app/services/api-consumption.service.ts
+++ src/app/services/api-consumption.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
 
 export interface Book {
   id: string;
@@ -21,35 +22,44 @@ export class ApiConsumptionService {
 
   constructor(private httpClient: HttpClient, private angularFirestore: AngularFirestore) {}
 
-  getRandomDog(): Observable<any> {
-    return this.httpClient.get(this.dogApiUrl);
+  getRandomDogs(dogs: number = 5): Observable<any> {
+    return this.httpClient.get(`${this.dogApiUrl}/${dogs}`);
   }
 
-  getRandomRobot(): string {
-    const randomNumber: number = Math.floor(Math.random() * 100);
-    return `${this.robotApiUrl}/${randomNumber}`;
+  getRandomRobots(robots: number = 5): string[] {
+    const robotImages: string[] = [];
+    for (let i = 0; i < robots; i++) {
+      const randomNumber = Math.floor(Math.random() * 1000);
+      robotImages.push(`${this.robotApiUrl}/${randomNumber}`);
+    }
+    return robotImages;
   }
 
   getRandomBooks(books: number = 10): Observable<any> {
-    let randomNumbers: number[] = [];
+    const randomNumbers: number[] = [];
     for (let i = 0; i < books; i++) {
-      const randomNumber = Math.floor(Math.random() * 100);
+      const randomNumber = Math.floor(Math.random() * 1000);
       randomNumbers.push(randomNumber);
     }
     const randomNumbersStr = randomNumbers.join(',');
 
-    return this.httpClient.get(`${this.bookApiUrl}/?ids=${randomNumbersStr}`);
+    return this.httpClient.get(`${this.bookApiUrl}`, { params: { ids: randomNumbersStr } });
   }
 
   async saveBooksGenerated(books: Book[]): Promise<any> {
+    const batch: firebase.firestore.WriteBatch = this.angularFirestore.firestore.batch();
+
     try {
-      for (let book of books) {
-        await this.angularFirestore.collection('books').add(book);
-      }
+      books.forEach((book) => {
+        const bookRef = this.angularFirestore.collection('books').doc().ref;
+        batch.set(bookRef, book);
+      });
+
+      await batch.commit();
     } catch (error) {
       console.log(error);
-      return 'Error al guardar los libros';
+      return 'Error al guardar el libro';
     }
-    return 'Libros guardados correctamente';
+    return 'Libro/s guardado/s correctamente';
   }
 }
