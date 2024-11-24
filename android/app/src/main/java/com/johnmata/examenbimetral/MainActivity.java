d75758a2e77745fbe7b41f0d01cc9ccea4701f46
JohnMata0427 <jhonmata0427@gmail.com>
2024-11-23 20:08:54 -0500

    Splash Screen and Icon

---
+++ android/app/src/main/java/com/johnmata/examenbimetral/MainActivity.java
@@ -0,0 +1,5 @@
+package com.johnmata.examenbimetral;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'examen-bimestral-01',
-  webDir: 'www'
+  appId: 'com.johnmata.examenbimetral',
+  appName: 'Examen Bimestral - John Mata',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#ffffff',
+      showSpinner: false,
+      androidSpinnerStyle: 'small',
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: false,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
